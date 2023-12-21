ab1ce6b06c4221a23ce5fa973b62f561bcf4fe54
Sreekanth Pasumarthy <pskanthu@gmail.com>
2023-12-20 21:31:37 -0800

    Moved ldd.mk to use module subdirs

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -14,10 +14,12 @@ LDD_SITE = 'git@github.com:cu-ecen-aeld/assignment-7-psk73.git'
 LDD_SITE_METHOD = git
 LDD_GIT_SUBMODULES = YES
 
-define LDD_BUILD_CMDS
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/misc-modules
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/scull
-endef
+#define LDD_BUILD_CMDS
+#	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/misc-modules
+#	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/scull
+#endef
+LDD_MODULE_SUBDIRS = misc-modules
+LDD_MODULE_SUBDIRS += scull
 
 # 
 define LDD_INSTALL_TARGET_CMDS
@@ -30,4 +32,5 @@ define LDD_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/initscript/ldd-start-stop $(TARGET_DIR)/etc/init.d/S98lddmodules
 endef
 
+$(eval $(kernel-module))
 $(eval $(generic-package))
