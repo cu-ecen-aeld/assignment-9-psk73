1d4dd31bb00b0a5967198f8e5d8093c09b578ae5
Sreekanth Pasumarthy <pskanthu@gmail.com>
2023-12-27 12:47:17 -0800

    Updated hash for assign3

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #PSK-DONE: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 'cdfcee5'
+AESD_ASSIGNMENTS_VERSION = '37c5821'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
